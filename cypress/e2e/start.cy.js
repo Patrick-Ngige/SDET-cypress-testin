f3ee5cb622a720c7c427327a6555cb98ad5322d3
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-01 16:25:53 +0300

    testing the example in cypress.io

--- cypress/e2e/start.cy.js
+++ cypress/e2e/start.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('testing the Jitu website', () => {
     cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+    cy.url().should('include', '/commands/actions')
+
+    cy.get('.action-email').type('fake@gamil.com')
+    cy.get('.action-email').should('have.value', 'fake@gmail.com')
   })
 })
