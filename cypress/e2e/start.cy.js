4e3ed031186fdc1923634403b308a566e860f258
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-04 14:08:57 +0300

    testing simple operations of the app

--- cypress/e2e/start.cy.js
+++ cypress/e2e/start.cy.js
@@ -5,7 +5,7 @@ describe('template spec', () => {
     cy.contains('type').click()
     cy.url().should('include', '/commands/actions')
 
-    cy.get('.action-email').type('fake@gamil.com')
+    cy.get('.action-email').type('fake@gmail.com')
     cy.get('.action-email').should('have.value', 'fake@gmail.com')
   })
 })
