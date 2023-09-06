45358679784206c645ed1984282aa01906e38e20
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-06 16:15:33 +0300

    changed the website to easy manage

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -1,10 +1,20 @@
-describe('CSS locators', () => {
-    it('CSS locators', () => {
+describe('easy manage login', () => {
+    it('Correct credentials', () => {
         cy
         .visit("http://localhost/easy-manage/")
         cy.get("[name='email']").type("wakemanja007@gmail.com")
 
         cy.get("[name='password']").type('easy-manage')
         cy.get("[name='login']").click()
+    })
+
+    it('Inorrect credentials', () => {
+        cy
+        .visit("http://localhost/easy-manage/")
+        cy.get("[name='email']").type("wakemanja007@gmail.com")
+
+        cy.get("[name='password']").type('easy-manag')
+        cy.get("[name='login']").click()
+        cy.get('.error-message > span').should('have.text', 'Invalid email or password.Please try again.')
     })
 })
