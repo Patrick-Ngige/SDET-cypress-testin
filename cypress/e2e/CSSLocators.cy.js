773fb2b87fdcc4781c64a57efce2ceda0f17e14c
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-06 15:13:46 +0300

    testing a website

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -1,5 +1,9 @@
 describe('CSS locators', () => {
     it('CSS locators', () => {
-        
+        cy
+        .visit("http://automationpractice.com/index.php")
+        cy.get("#search_query_top").types("T-Shirts")
+        cy.get("[name='submit_search']").click()
+        cy.get(".lighter").contains("T-Shirts")
     })
 })
