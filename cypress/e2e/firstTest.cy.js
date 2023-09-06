31c12c5b166cfa1f279f573cae00f024a715b409
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-06 13:36:43 +0300

    adding test2 a negative test

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,6 +1,10 @@
 describe("First test", () => {
-    it("test 1", () => {
+    it("test1 (+ve)", () => {
         cy.visit("https://opensource-demo.orangehrmlive.com/")
         cy.title().should('eq','OrangeHRM')
+    })
+    it("test2 (-ve)", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+        cy.title().should('eq','OrangeHRM1')
     })
 })
