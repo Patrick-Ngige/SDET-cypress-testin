b647d65b641e5003e74d1015f35f2620877befaa
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-07 15:43:29 +0300

    utilizing beforeEach function

--- cypress/e2e/nextJsApp.cy.js
+++ cypress/e2e/nextJsApp.cy.js
@@ -1,10 +1,11 @@
 describe('Fundamentals test', () => {
-    it('contains correct header text', () => {
+    beforeEach(() => {
         cy.visit("/fundamentals")
+    })
+    it('contains correct header text', () => {
         cy.get('[data-test="fundamentals-header"]').should('have.text', 'Testing Fundamentals')
     })
-    it('Accordion works correctly', () => {
-        cy.visit("/fundamentals")
+    it('Accordion works correctly', () => { 
         cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
         cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
         cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
