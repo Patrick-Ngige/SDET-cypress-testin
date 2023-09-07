3d20bd0358ca2af9084e62520147593b85f01ef9
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-07 16:28:04 +0300

    testing subscribe button

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,5 +4,9 @@ describe('form tests', () => {
     })
         it('Test subscribe form', () => {
             cy.contains(/Testing Forms/i)
+            cy.getDataTest('subscribe-form').find('input').type('ryan@coderyan.com')
+            cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should('not.exist')
+            cy.getDataTest('subscribe-button').click()
+            cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should('be.exist')
         })
 })
