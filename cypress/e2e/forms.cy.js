fc15c158da13241dae63317e5c1f05033b0bf2dc
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-07 16:39:13 +0300

    using alias

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,9 +4,13 @@ describe('form tests', () => {
     })
         it('Test subscribe form', () => {
             cy.contains(/Testing Forms/i)
-            cy.getDataTest('subscribe-form').find('input').type('ryan@coderyan.com')
+            cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+            cy.get('@subscribe-input').type('ryan@coderyan.com')
             cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should('not.exist')
             cy.getDataTest('subscribe-button').click()
-            cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should('be.exist')
+            cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should('exist')
+
+
+            
         })
 })
