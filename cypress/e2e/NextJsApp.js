353c69e7678f1933a7aac0f96019de9e1b369f74
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-07 15:16:08 +0300

    testing the h1

--- cypress/e2e/NextJsApp.js
+++ cypress/e2e/NextJsApp.js
@@ -1,8 +1,8 @@
 describe('Fundamentals test', () => {
     it('passes', () => {
-        cy
-        .visit("http://localhost:3000/fundamentals")
-    
+    cy
+    .visit("http://localhost:3000/fundamentals")
+    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
     })
 
 })
