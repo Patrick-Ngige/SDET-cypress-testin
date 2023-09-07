6da19ac51570d109735d7720bed2f05c28d877b8
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-07 14:55:46 +0300

    testing a next.js app

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -1,22 +1,8 @@
-describe('easy manage login', () => {
-    it('Correct credentials', () => {
+describe('Fundamentals test', () => {
+    it('passes', () => {
         cy
-        .visit("http://localhost/easy-manage/")
-        cy.get("[name='email']").type("wakemanja007@gmail.com")
-
-        cy.get("[name='password']").type('easy-manage')
-        cy.get("[name='login']").click()
+        .visit("http://localhost:3000/fundamentals")
+    
     })
 
-    it('Inorrect credentials', () => {
-        cy
-        .visit("http://localhost/easy-manage/")
-        cy.get("[name='email']").type("wakemanja007@gmail.com")
-
-        cy.get("[name='password']").type('easy-manag')
-        cy.get("[name='login']").click()
-        // cy.url().should('have.text','/easy-manage/admin-pm-list')
-        // cy.get('.error-message > span').should('have.text', 'Invalid email or password.Please try again.')
-        // cy.get('.attempts').should('have.text', "Please wait 60 seconds before trying again.")
-    })
 })
