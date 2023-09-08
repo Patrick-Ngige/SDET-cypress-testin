4f48703ce4a378e5a23204d10b9f5d939189274b
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-08 15:33:17 +0300

    changing eq to equal

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,27 +1,27 @@
-describe('Test various examples', () => {
-    beforeEach(() => {
-        cy.visit('/examples')
-    })
-    it('multi-page testing', () => {
-        cy.getDataTest('nav-why-cypress').click();
-        cy.location("pathname").should("equal", "/")
+describe("Test various examples", () => {
+  beforeEach(() => {
+    cy.visit("/examples");
+  });
+  it("multi-page testing", () => {
+    cy.getDataTest("nav-why-cypress").click();
+    cy.location("pathname").should("equal", "/");
 
-        cy.getDataTest('nav-overview').click();
-        cy.location("pathname").should("equal", "/overview")
+    cy.getDataTest("nav-overview").click();
+    cy.location("pathname").should("equal", "/overview");
 
-        cy.getDataTest('nav-fundamentals').click();
-        cy.location("pathname").should("equal", "/fundamentals")
+    cy.getDataTest("nav-fundamentals").click();
+    cy.location("pathname").should("equal", "/fundamentals");
 
-        cy.getDataTest('nav-examples').click();
-        cy.location("pathname").should("equal", "/examples")
+    cy.getDataTest("nav-examples").click();
+    cy.location("pathname").should("equal", "/examples");
 
-        cy.getDataTest('nav-forms').click();
-        cy.location("pathname").should("equal", "/forms")
+    cy.getDataTest("nav-forms").click();
+    cy.location("pathname").should("equal", "/forms");
 
-        cy.getDataTest('nav-component').click();
-        cy.location("pathname").should("equal", "/component")
+    cy.getDataTest("nav-component").click();
+    cy.location("pathname").should("equal", "/component");
 
-        cy.getDataTest('nav-best-practices').click();
-        cy.location("pathname").should("equal", "/best-practices")
-    })
-})
+    cy.getDataTest("nav-best-practices").click();
+    cy.location("pathname").should("equal", "/best-practices");
+  });
+});
