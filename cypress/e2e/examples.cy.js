4c7810144d1c7bf456903e70101dda462ddc6080
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-08 15:23:34 +0300

    changing eq to equal

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -4,24 +4,24 @@ describe('Test various examples', () => {
     })
     it('multi-page testing', () => {
         cy.getDataTest('nav-why-cypress').click();
-        cy.location("pathname").should("eq", "/")
+        cy.location("pathname").should("equal", "/")
 
         cy.getDataTest('nav-overview').click();
-        cy.location("pathname").should("eq", "/overview")
+        cy.location("pathname").should("equal", "/overview")
 
         cy.getDataTest('nav-fundamentals').click();
-        cy.location("pathname").should("eq", "/fundamentals")
+        cy.location("pathname").should("equal", "/fundamentals")
 
         cy.getDataTest('nav-examples').click();
-        cy.location("pathname").should("eq", "/examples")
+        cy.location("pathname").should("equal", "/examples")
 
         cy.getDataTest('nav-forms').click();
-        cy.location("pathname").should("eq", "/forms")
+        cy.location("pathname").should("equal", "/forms")
 
         cy.getDataTest('nav-component').click();
-        cy.location("pathname").should("eq", "/component")
+        cy.location("pathname").should("equal", "/component")
 
         cy.getDataTest('nav-best-practices').click();
-        cy.location("pathname").should("eq", "/best-practices")
+        cy.location("pathname").should("equal", "/best-practices")
     })
 })
