ac85dcd95b0925991b4112672289d0b58e85ee7b
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-08 15:12:03 +0300

    writing example tests

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,5 +1,10 @@
 describe('Test various examples', () => {
+    beforeEach(() => {
+        cy.visit('/examples')
+    })
     it('multi-page testing', () => {
-        cy.visit('/').pause()
+        cy.getDataTest('nav-why-cypress').click();
+        cy.location("pathname").should("eq", "/")
+
     })
 })
