3f6f64324fcaac3f0807672a22f022978c6de72c
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-12 21:30:29 +0300

    checking weather the grudges have been added or not

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -54,7 +54,7 @@ describe("Test various examples", () => {
       cy.get("li").should("have.length", 1);
     });
 
-    cy.getDataTest('grudge-list-title').should('have.text', 'Add Some Grudges')
+    cy.getDataTest('grudge-list-title').should('have.text', 'Grudges')
 
     cy.getDataTest("grudge-input").within(() => {
       cy.get("input").type("number 2");
